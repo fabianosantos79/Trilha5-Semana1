6c56ae29f561700fa39118b992c0e4b2d4d6f711
Fabiano <fabiano.ssantos79@gmail.com>
2024-04-23 16:22:46 -0300

    Formulas Matematicas

---
+++ semana1/Formulas Matematicas/script.js
@@ -0,0 +1,25 @@
+// 1. crie uma variável para cada figura geométrica
+let base;
+let altura;
+base = 5;
+altura = 10;
+let areaRetangulo = parseInt(base * altura);
+console.log("A area do Retângulo é: " + areaRetangulo);
+
+
+let raio;
+raio = 5;
+let areaCirculo = Math.PI * (raio * raio);
+console.log("A área do Círculo é: " + areaCirculo);
+
+let base2;
+let altura2;
+base2 = 15;
+altura2 = 5;
+let areaTriangulo = (base2 * altura2) / 2;
+console.log("A área do Triângulo é: " + Math.round(areaTriangulo));
+
+// 2. Atribua valores em váriaveis para as bases e calcule
+// a) a área do retângulo
+// b) a área do círculo
+// c) a área do triângulo retângulo
