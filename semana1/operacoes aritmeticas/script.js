79eb216c05e772a6c8b5ac49b51299da3eafd3bf
Fabiano <fabiano.ssantos79@gmail.com>
2024-04-23 02:09:12 -0300

    Operações aritméticas ➕

---
+++ semana1/operacoes aritmeticas/script.js
@@ -0,0 +1,25 @@
+let inteiro = 7
+let decimal = 3.5
+
+let adicao = inteiro + decimal
+let subtracao = inteiro - decimal
+let potenciacao = (adicao - .5) ** 2
+let expressao1 = inteiro - 4 / 2 / 2
+let expressao2 = (inteiro - 4) / 2
+let expressao3 = 2 + subtracao * 2
+let expressao4 = (2 + subtracao) * 2
+let modulo1 = potenciacao % 2
+let modulo2 = (inteiro - 1) % 2
+
+document.write("<strong>Número inteiro = </strong>" + inteiro + "<br>")
+document.write("<strong>Número decimal = </strong>" + decimal + "<hr>")
+
+document.write("<strong>Adição = </strong>" + adicao + " <br>")
+document.write("<strong>Subtração = </strong>" + subtracao + " <br>")
+document.write("<strong>Potenciação = </strong>" + potenciacao + " <br>")
+document.write("<strong>Expressão 1 = </strong>" + expressao1 + " <br>")
+document.write("<strong>Expressão 2 = </strong>" + expressao2 + " <br>")
+document.write("<strong>Expressão 3 = </strong>" + expressao3 + " <br>")
+document.write("<strong>Expressão 4 = </strong>" + expressao4 + " <br>")
+document.write("<strong>Módulo 1 = </strong>" + modulo1 + " <br>")
+document.write("<strong>Módulo 2 = </strong>" + modulo2 + " <br>")
